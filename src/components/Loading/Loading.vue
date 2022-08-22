02fdf42ca6fece6d1e099ff4adf8dee7d0d4d756
zifeifish <congy_hu@163.com>
2022-08-22 23:31:26 +0800

    Loading Plugin

---
+++ src/components/Loading/Loading.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="loading-box" v-show="show">
+    <div class="loading-content">
+      <a-spin :tip="text"></a-spin>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loading",
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+    text: {
+      type: String,
+      default: "正在加载中...",
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+.loading-box {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background-color: rgba(255, 255, 255, 0.5);
+  z-index: 10;
+  .loading-content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
